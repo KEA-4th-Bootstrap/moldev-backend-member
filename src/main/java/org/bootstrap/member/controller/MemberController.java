57aaa231b0812a7b4442171d467da6ce79012b10
ziiyouth <ziy913@naver.com>
2024-04-17 17:11:46 +0900

    KBB-31 [feat] 내 프로필 조회 API controller 생성

--- src/main/java/org/bootstrap/member/controller/MemberController.java
+++ src/main/java/org/bootstrap/member/controller/MemberController.java
@@ -1,7 +1,12 @@
 package org.bootstrap.member.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.bootstrap.member.common.SuccessResponse;
+import org.bootstrap.member.dto.response.MemberProfileResponseDto;
 import org.bootstrap.member.service.MemberService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MemberController {
     private final MemberService memberService;
+//    private AuthorizationExtractor authExtractor;
 
+    @GetMapping("/my")
+    public ResponseEntity<SuccessResponse<?>> getPost(@RequestHeader("Authorization") String authHeader) {
+        Long memberId = Long.parseLong(authHeader);
+        final MemberProfileResponseDto responseDto = memberService.getMemberProfile(memberId);
+        return SuccessResponse.ok(responseDto);
+    }
 }
