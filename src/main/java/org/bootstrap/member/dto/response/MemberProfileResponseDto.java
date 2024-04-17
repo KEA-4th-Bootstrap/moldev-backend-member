52765a110a37f70ed6e79d1be31c26275a7b799f
ziiyouth <ziy913@naver.com>
2024-04-17 17:12:02 +0900

    KBB-31 [feat] 내 프로필 조회 API dto 생성

---
+++ src/main/java/org/bootstrap/member/dto/response/MemberProfileResponseDto.java
@@ -0,0 +1,25 @@
+package org.bootstrap.member.dto.response;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import org.bootstrap.member.entity.Member;
+
+@Builder(access = AccessLevel.PRIVATE)
+public record MemberProfileResponseDto(
+        String profileImgUrl,
+        String moldevId,
+        String email,
+        String nickname,
+        String islandName
+) {
+    public static MemberProfileResponseDto of (Member member){
+        return MemberProfileResponseDto.builder()
+                .profileImgUrl(member.getProfileImgUrl())
+                .moldevId(member.getMoldevId())
+                .email(member.getEmail())
+                .nickname(member.getNickname())
+                .islandName(member.getIslandName())
+                .build();
+    }
+
+}
