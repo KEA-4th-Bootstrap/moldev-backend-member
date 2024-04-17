83ba64b303aaed544bbfea242c61b1c04f5c5f1e
ziiyouth <ziy913@naver.com>
2024-04-17 17:11:29 +0900

    KBB-31 [feat] errorcode 생성

--- src/main/java/org/bootstrap/member/common/error/GlobalErrorCode.java
+++ src/main/java/org/bootstrap/member/common/error/GlobalErrorCode.java
@@ -20,7 +20,7 @@ public enum GlobalErrorCode implements BaseErrorCode {
 
     /* S3 */
     INVALID_FILE_UPLOAD(BAD_REQUEST, "S3_400_1", "파일을 업로드할 수 없습니다."),
-    INVALID_MULTIPART_FILE(BAD_REQUEST, "S3_400_2", "잘못된 Multipart 입니다."),
+    INVALID_MULTIPART_FILE(BAD_REQUEST, "S3_400_2", "잘못된 Multipart 입니다.")
     ;
 
     private HttpStatus status;
