bbfcbf4afd7d5d9f0402bba98bacbd2e89880f5b
ziiyouth <ziy913@naver.com>
2024-04-17 19:54:15 +0900

    KBB-32 [feat] member 이름/섬 이름 수정 API service 구현

--- src/main/java/org/bootstrap/member/service/MemberService.java
+++ src/main/java/org/bootstrap/member/service/MemberService.java
@@ -1,6 +1,7 @@
 package org.bootstrap.member.service;
 
 import lombok.RequiredArgsConstructor;
+import org.bootstrap.member.dto.request.ProfilePatchRequestDto;
 import org.bootstrap.member.dto.response.MemberProfileResponseDto;
 import org.bootstrap.member.entity.Member;
 import org.bootstrap.member.repository.MemberRepository;
@@ -18,6 +19,11 @@ public class MemberService {
     public MemberProfileResponseDto getMemberProfile(Long memberId) {
         Member member = findByIdOrThrow(memberId);
         return MemberProfileResponseDto.of(member);
+    }
+
+    public void patchMemberProfile(Long memberId, ProfilePatchRequestDto profilePatchRequestDto){
+        Member member = findByIdOrThrow(memberId);
+        member.updateProfile(profilePatchRequestDto);
     }
 
     private Member findByIdOrThrow(Long memberId) {
