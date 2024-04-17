153376a3a6f3ca3c7fa81ea899a95613e96e0f56
ziiyouth <ziy913@naver.com>
2024-04-17 17:11:52 +0900

    KBB-31 [feat] 내 프로필 조회 API service 생성

--- src/main/java/org/bootstrap/member/service/MemberService.java
+++ src/main/java/org/bootstrap/member/service/MemberService.java
@@ -1,13 +1,28 @@
 package org.bootstrap.member.service;
 
 import lombok.RequiredArgsConstructor;
+import org.bootstrap.member.dto.response.MemberProfileResponseDto;
+import org.bootstrap.member.entity.Member;
+import org.bootstrap.member.repository.MemberRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.bootstrap.member.exception.MemberNotFoundException;
 
 @RequiredArgsConstructor
 @Transactional
 @Service
 public class MemberService {
+    
+    private final MemberRepository memberRepository;
 
+    public MemberProfileResponseDto getMemberProfile(Long memberId) {
+        Member member = findByIdOrThrow(memberId);
+        return MemberProfileResponseDto.of(member);
+    }
+
+    private Member findByIdOrThrow(Long memberId) {
+        return memberRepository.findById(memberId)
+                .orElseThrow(() -> MemberNotFoundException.EXCEPTION);
+    }
 
 }
