cdb345752ba050c82302fe4d59ade67ea7abfbf4
kdhhuns2000 <52370750+kdhhuns2000@users.noreply.github.com>
2024-05-04 19:22:32 +0900

    KBB-51 [Feat] 섬 조회 수 증가 기능 구현

--- src/main/java/org/bootstrap/member/service/MemberService.java
+++ src/main/java/org/bootstrap/member/service/MemberService.java
@@ -1,5 +1,8 @@
 package org.bootstrap.member.service;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.bootstrap.member.aws.S3Service;
 import org.bootstrap.member.dto.request.PasswordCheckRequestDto;
@@ -10,22 +13,34 @@ import org.bootstrap.member.dto.response.MyProfileResponseDto;
 import org.bootstrap.member.entity.Member;
 import org.bootstrap.member.exception.PasswordWrongException;
 import org.bootstrap.member.repository.MemberRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.bootstrap.member.exception.MemberNotFoundException;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneOffset;
+import java.util.Arrays;
 import java.util.Objects;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Transactional
 @Service
 public class MemberService {
-    
+
+    private static final Logger log = LoggerFactory.getLogger(MemberService.class);
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
     private final S3Service s3Service;
+    private final RedisTemplate<String, String> redisTemplate;
     public static final String PROFILE_IMAGE_DIRECTORY = "profile";
 
     public MyProfileResponseDto getMyProfile(Long memberId) {
@@ -58,6 +73,31 @@ public class MemberService {
         Member member = findByIdOrThrow(memberId);
         String profileImgUrl = checkProfileImageAndGetUrl(profileImage, member.getMoldevId());
         member.updateProfileImage(profileImgUrl);
+    }
+
+    public void viewCountUpByCookie(Long memberId, HttpServletRequest request, HttpServletResponse response){
+        Member member = findByIdOrThrow(memberId);
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+
+        Cookie[] cookies = Optional.ofNullable(request.getCookies()).orElseGet(() -> new Cookie[0]);
+        Cookie cookie = Arrays.stream(cookies)
+                .filter(c -> c.getName().equals("viewCount"))
+                .findFirst()
+                .orElseGet(() -> {
+                    valueOperations.increment(String.valueOf(member.getId()), 1L);
+                    return new Cookie("viewCount", String.valueOf(member.getId()));
+                });
+
+        if (!cookie.getValue().contains(String.valueOf(member.getId()))){
+            valueOperations.increment(String.valueOf(member.getId()), 1L);
+            cookie.setValue(cookie.getValue() + String.valueOf(member.getId()));
+        }
+
+        long todayEndSecond = LocalDate.now().atTime(LocalTime.MAX).toEpochSecond(ZoneOffset.UTC);
+        long currentSecond = LocalDateTime.now().toEpochSecond(ZoneOffset.UTC);
+        cookie.setPath("/");
+        cookie.setMaxAge((int) (todayEndSecond - currentSecond));
+        response.addCookie(cookie);
     }
 
     private void validatePassword(String inputPassword, String encodedPassword) {
