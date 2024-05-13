7fe03e515bb1d0420412e30981460ad7440a4658
RyuKwanKon <rhks33rhs@gmail.com>
2024-05-13 15:14:00 +0900

    KBB-91 [feat] moldevId로 member 조회 메서드 작성

--- src/main/java/org/bootstrap/member/repository/MemberRepository.java
+++ src/main/java/org/bootstrap/member/repository/MemberRepository.java
@@ -3,5 +3,8 @@ package org.bootstrap.member.repository;
 import org.bootstrap.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByMoldevId(String moldevId);
 }
