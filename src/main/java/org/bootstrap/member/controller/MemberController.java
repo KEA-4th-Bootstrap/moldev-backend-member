2ec6adea238d35d26d053cb3f177e53b372ea336
kdhhuns2000 <52370750+kdhhuns2000@users.noreply.github.com>
2024-05-13 17:24:17 +0900

    KBB-87 [Feat] pagedefault 추가 (size=10으로 설정)

--- src/main/java/org/bootstrap/member/controller/MemberController.java
+++ src/main/java/org/bootstrap/member/controller/MemberController.java
@@ -14,6 +14,7 @@ import org.bootstrap.member.dto.response.MyProfileResponseDto;
 import org.bootstrap.member.service.MemberService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -89,7 +90,7 @@ public class MemberController {
     @GetMapping("/admin")
     public ResponseEntity<SuccessResponse<?>> getMembersInfoForAdmin(@RequestParam(required = false) Boolean marketingAgree,
                                                                      @RequestParam(required = false) String search,
-                                                                     Pageable pageable) {
+                                                                     @PageableDefault Pageable pageable) {
         Page<MemberInfoForAdminResponseDto> membersInfoForAdmin = memberService.getMembersInfoForAdmin(marketingAgree, search, pageable);
         return SuccessResponse.ok(membersInfoForAdmin);
     }
