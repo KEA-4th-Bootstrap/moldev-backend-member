69e0f1eed8f66457eb228c0fdf38f57435853a74
OJOJIN <ojy101039@gmail.com>
2024-06-03 13:44:06 +0900

    KBB-133 [Feat] 유저 여러 moldevId로 검색하기 추가

--- src/main/java/org/bootstrap/member/repository/MemberRepository.java
+++ src/main/java/org/bootstrap/member/repository/MemberRepository.java
@@ -3,6 +3,7 @@ package org.bootstrap.member.repository;
 import org.bootstrap.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long>, MemberQueryRepository {
